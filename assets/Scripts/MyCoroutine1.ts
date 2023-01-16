ae3a1e80a10b53b24199d8c4c102fb1c99e28a8f
liobio <582437980@qq.com>
2023-01-16 17:23:06 +0800

    fix 协程2

--- assets/Scripts/MyCoroutine1.ts
+++ assets/Scripts/MyCoroutine1.ts
@@ -3,12 +3,7 @@ const { ccclass, property } = _decorator;
 
 @ccclass('MyCoroutine1')
 export class MyCoroutine1 extends Component {
-    start() {
 
-    }
-
-    update(deltaTime: number) {
-        
-    }
 }
-
+
+
