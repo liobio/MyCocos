6a50551922cc46cc111e902da770ddb79b215474
liobio <582437980@qq.com>
2023-01-16 19:02:34 +0800

    no message

--- assets/Scripts/MyCoroutine.ts
+++ assets/Scripts/MyCoroutine.ts
@@ -38,6 +38,57 @@ export class MyCoroutine extends Component {
 
 
     }
+    //     using System.Collections;
+    // using UnityEngine;
+    // using UnityEngine.UI;
+
+    //     public class ComboController: MonoBehaviour
+    // {
+    //     public Text comboText;
+    //     public bool isComboTime;
+    //     public float intervalTime;
+    //     public float comboTimeLimit;
+    //     public int currentComboCount;
+
+    //     void Start()
+    //     {
+
+    //     }
+
+    //     // Update is called once per frame
+    //     void Update()
+    //     {
+    //         if (Input.GetKeyDown(KeyCode.Space)) {
+
+    //             StartCombo();
+    //             comboText.gameObject.SetActive(true);
+    //         }
+    //     }
+    //     public void StartCombo()
+    //     {
+    //         if (isComboTime) {
+    //             intervalTime = 0f;
+    //         }
+    //         else {
+    //             currentComboCount = 0;
+    //             StartCoroutine(ComboTimer());
+    //         }
+    //         currentComboCount++;
+    //         comboText.text = currentComboCount.ToString() + " COMBO";
+    //     }
+    //     IEnumerator ComboTimer()
+    //     {
+    //         isComboTime = true;
+    //         intervalTime = 0;
+    //         while (intervalTime <= comboTimeLimit) {
+    //             intervalTime += Time.deltaTime;
+    //             yield return null;
+    //         }
+    //         isComboTime = false;
+    //         comboText.gameObject.SetActive(false);
+    //     }
+
+    // }
 
 
 }
