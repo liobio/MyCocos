5bbb16c42b0e97e396e3c446267c9c62adb07921
liobio <582437980@qq.com>
2023-01-16 17:26:27 +0800

    Merge branch 'heads/head2' into dev
    # Conflicts:
    #	assets/Scripts/MyCoroutine.ts.meta

---
+++ assets/Scripts/MyCoroutine1.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('MyCoroutine1')
+export class MyCoroutine1 extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
