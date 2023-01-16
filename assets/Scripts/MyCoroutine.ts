88b41bb10ffc8b449f328517f8e0f1cfeeff979d
liobio <582437980@qq.com>
2023-01-16 16:07:59 +0800

    协程1-begin

---
+++ assets/Scripts/MyCoroutine.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('MyCoroutine')
+export class MyCoroutine extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
